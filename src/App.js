71ba08a85f17a0ca9411bbd4ff2bb51ea01f56bc
Deeksha-14 <deekshakushwah0@gmail.com>
2025-09-10 17:40:14 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { ColorModeContext } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import
+
+
+import { useMode } from "./theme";
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+
+
+
+
 function App() {
-  return (
-    <div className="app">
-      
+
+  const [theme, colorMode] = useMode();
+  return(
+
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <div className="app">  
+      <main className="content"></main>
+      <Topbar/>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+  
   );
 }
 
